498c556dc49a7eb1253556cc497faa5d64676523
Janos Perge <janos.perge@innonic.com>
2017-12-04 15:58:36 +0100

    create test project

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,23 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
+var fs = require('fs');
 
 gulp.task('default', function () {
-    var templateData = {
-            firstName: 'John'
-        },
-        options = {
-            ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
-            partials : {
-                footer : '<footer>the end</footer>'
-            },
-            batch : ['./src/partials'],
+    var content = fs.readFileSync('src/templateData/product/templateData.json');
+    var templateData = JSON.parse(content);
+
+    var options = {
+            batch : ['./src/templates/product/partials'],
             helpers : {
                 capitals : function(str){
                     return str.toUpperCase();
                 }
             }
-        }
+        };
     
-    return gulp.src('src/hello.hbs')
+    return gulp.src('src/templates/product/product.hbs')
     .pipe(handlebars(templateData, options))
-    .pipe(rename('hello.html'))
+    .pipe(rename('product.html'))
     .pipe(gulp.dest('dist'));
 });
